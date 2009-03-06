2630ec8bb8d9a7d3adc91093bf61cc5788696fda
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-03-06 15:17:51 +0000

    add basic

---
+++ basic/structTest.c
@@ -0,0 +1,35 @@
+/*
+*/
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+typedef struct _name_t {
+    char lastName[25];
+    char firstName[25];
+} name_t;
+
+int dumpName(const name_t *tmp);
+ 
+int main(int argc, char *argv[]) {
+
+    name_t one, two;
+
+    strcpy(one.lastName, "Zhang");
+    strcpy(one.firstName, "Kevin");
+    dumpName(&one);
+
+    two = one;
+    dumpName(&two);
+
+    strcpy(two.lastName, "Yin");
+    dumpName(&two);
+
+    return 0;
+}
+
+int dumpName(const name_t *tmp) {
+    printf("first: [%s], last: [%s]\n", tmp->firstName, tmp->lastName);
+    return 0;
+}
+
