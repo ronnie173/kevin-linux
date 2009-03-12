24f4c43fb544b933e8537cdd3546876d7cc83f10
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-03-12 09:58:37 +0000

    new c

--- basic/arrayTest.c
+++ basic/arrayTest.c
@@ -5,6 +5,8 @@
 #define MAX 10
 int main(int argc, char *argv[]) {
     int i = 2, a[MAX], j = 23;
+    char *p = "hello world.";
+    char *q = "hello world.";
     
     if (&i == &a[MAX]) {
         printf("a[MAX] equals i\n");
@@ -16,5 +18,13 @@ int main(int argc, char *argv[]) {
         printf("a[MAX] value is %d\n", a[MAX]);
     }
     
+    printf("p is [%s]\n", p);
+    printf("q is [%s]\n", q);
+    
+    p[4] = '\0';
+    
+    printf("p is [%s]\n", p);
+    printf("q is [%s]\n", q);
+
     return 0;
 }
