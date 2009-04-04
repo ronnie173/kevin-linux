b684886e3de4d05edb1755f222d60e700af53e1d
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-04-04 16:32:47 +0000

    fix segment fault bug

--- trunk/SortTest/quicksort.c
+++ trunk/SortTest/quicksort.c
@@ -1,10 +1,14 @@
-/* this program has segment fault bug */
+/*
+ * original randint may generate negative int which will
+ * cause segment fault bug
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define MAXN 10000000
+#define MAXRAND 32767
 
 typedef int DType;
 
@@ -14,12 +18,16 @@ int n;
 
 void swap(int i, int j) {
     DType t = x[i];
+
+    //printf("swap i=%d j=%d\n", i, j);
+
     x[i] = x[j];
     x[j] = t;
 }
 
 int randint(int l, int u) {
-    return l + (RAND_MAX * rand() + rand()) % (u-l+1);
+    //return l + (MAXRAND * rand() + rand()) % (u - l + 1);
+    return l + rand() % (u - l + 1);
 }
 
 void qsort1(int l, int u) {
@@ -36,16 +44,21 @@ void qsort1(int l, int u) {
 }
 
 void quicksort(int l, int u) {
-    int i, m;
+    int i, m, rand;
+
+    //printf("l=%d u=%d\n", l, u);
 
     if (l >= u) return;
 
-    swap(l, randint(l, u));
+    rand = randint(l, u);
+    //printf("rand is %d\n", rand);
+    swap(l, rand);
     m = l;
 
-    for (i = l + 1; i <= u; i++)
+    for (i = l + 1; i <= u; i++) {
         if (x[i] < x[l])
             swap(++m, i);
+    }
 
     swap(l, m);
     quicksort(l, m - 1);
@@ -55,6 +68,9 @@ void quicksort(int l, int u) {
 int main() {
     int i, mod, start, clicks;
 
+    printf("rand max is %d\n", RAND_MAX);
+    printf("rand value is %d\n", rand());
+
     scanf("%d %d", &n, &mod);
     if (mod <= 0)
         mod = 10 * n ;
@@ -62,13 +78,13 @@ int main() {
         x[i] = randint(0, mod - 1);
 
     start = clock();
-    //quicksort(0, n - 1);
-    qsort1(0, n - 1);
+    quicksort(0, n - 1);
+    //qsort1(0, n - 1);
 
     clicks = clock() - start;
 
     for (i = 0; i < n - 1; i++) {
-        printf("x[%d]=%d\n", i, x[i]);
+        //printf("x[%d]=%d\n", i, x[i]);
         if (x[i] > x[i + 1])
             printf("  SORT BUG i=%d\n", i);
     }
