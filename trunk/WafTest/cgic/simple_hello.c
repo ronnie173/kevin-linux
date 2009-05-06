1db26d1bf919ee193e5c6cb5b8ced2bc91e374ab
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-05-06 05:47:10 +0000

    fix bug

--- trunk/WafTest/cgic/simple_hello.c
+++ trunk/WafTest/cgic/simple_hello.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	printf("Content-Type: text/hmtl\n\n");
-	printf("<html><head><title>Simple Hello</title></head>\n");
-	printf("<body><h1>Simple Hello</h1></body>\n");
+    printf("Content-Type: text/html\n\n");
+    printf("<html><head><title>Simple Hello</title></head>\n");
+    printf("<body><h1>Simple Hello</h1></body></html>\n");
 
-	return 0;
+    return 0;
 }
 
