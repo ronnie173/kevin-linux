a6c0a40cce604e3ab099a16d86530372c8bc910a
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-05-06 05:47:10 +0000

    fix bug

--- WafTest/cgic/test_post.c
+++ WafTest/cgic/test_post.c
@@ -14,36 +14,35 @@
 void HandleSubmit();
 
 int cgiMain() {
-	/* Send the content type, letting the browser know this is HTML */
-	cgiHeaderContentType("text/html");
+    /* Send the content type, letting the browser know this is HTML */
+    cgiHeaderContentType("text/html");
 
     fprintf(cgiOut, "<html><body><h1>hello world</h1></body></html>");
-#if 0
-	/* Top of the page */
-	fprintf(cgiOut, "<HTML><HEAD>\n");
-	fprintf(cgiOut, "<TITLE>cgic test</TITLE></HEAD>\n");
-	fprintf(cgiOut, "<BODY><H1>cgic test</H1>\n");
-	/* If a submit button has already been clicked, act on the 
-		submission of the form. */
-	if (cgiFormSubmitClicked("OK") == cgiFormSuccess) {
-		HandleSubmit();
-		fprintf(cgiOut, "<hr>\n");
-	}
 
-	/* Finish up the page */
-	fprintf(cgiOut, "</BODY></HTML>\n");
-#endif
+    /* Top of the page */
+    fprintf(cgiOut, "<HTML><HEAD>\n");
+    fprintf(cgiOut, "<TITLE>cgic test</TITLE></HEAD>\n");
+    fprintf(cgiOut, "<BODY><H1>cgic test</H1>\n");
+    /* If a submit button has already been clicked, act on the 
+        submission of the form. */
+    if (cgiFormSubmitClicked("OK") == cgiFormSuccess) {
+        HandleSubmit();
+        fprintf(cgiOut, "<hr>\n");
+    }
 
-	return 0;
+    /* Finish up the page */
+    fprintf(cgiOut, "</BODY></HTML>\n");
+
+    return 0;
 }
 
 void HandleSubmit() {
-	char tmpStr[255];
+    char tmpStr[255];
     size_t strLen = 0;
 
-	cgiFormStringNoNewlines("username", tmpStr, 255);
+    cgiFormStringNoNewlines("username", tmpStr, 255);
     strLen = strlen(tmpStr);
-	fprintf(cgiOut, "<p>username len is %d</p>", strLen);
+    fprintf(cgiOut, "<p>username len is %d</p>", strLen);
 
     cgiFormStringNoNewlines("password", tmpStr, 255);
     strLen = strlen(tmpStr);
@@ -73,5 +72,5 @@ void HandleSubmit() {
     strLen = strlen(tmpStr);
     fprintf(cgiOut, "<p>phone len is %d</p>", strLen);
 }
-	
+
 
