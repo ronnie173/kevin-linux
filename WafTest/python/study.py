d3aa42ed9ed9b01c02879476657dc5abaf61b2f2
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-05-08 10:32:59 +0000

    update

--- WafTest/python/study.py
+++ WafTest/python/study.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
+def bar(x):
+    try:
+        return 1.0 / x
+    except ZeroDivisionError, message:
+        print "Can't divide by zero:", message
+        
+def test(a, b):
+    "this is a test function"
+    while a != 0:
+        a, b = b % a, a
+    return b
 
 def main():
     if 0 :
         print "in if statement\n"
         print "out if statement\n"
 
-    test()
+    b = test(12, 20)
+    print "b is ", b, "\n"
+    
+    print bar(3), "\n"
+    print bar(0), "\n"
+    
     print "end of program\n"
-main()
 
-def test():
-    print "in test function\"
-    return
-test()
+main()
