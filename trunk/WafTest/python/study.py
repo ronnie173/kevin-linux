b5eea45a7382a54dd6788e39b1eb809ed6113f59
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-05-08 05:26:51 +0000

    more python code

--- trunk/WafTest/python/study.py
+++ trunk/WafTest/python/study.py
@@ -5,5 +5,11 @@ def main():
         print "in if statement\n"
         print "out if statement\n"
 
+    test()
     print "end of program\n"
 main()
+
+def test():
+    print "in test function\"
+    return
+test()
