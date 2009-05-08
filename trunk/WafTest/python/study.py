e4bbbdb55bcca595b269798a62c2c471d7cd149e
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-05-08 05:20:29 +0000

    more python code

---
+++ trunk/WafTest/python/study.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+def main():
+    if 0 :
+        print "in if statement\n"
+        print "out if statement\n"
+
+    print "end of program\n"
+main()
--- trunk/WafTest/python/test_post.py
+++ trunk/WafTest/python/test_post.py
@@ -3,13 +3,57 @@ import cgi
 
 def main():
     print "Content-type: text/html\n"
+    
+    strlen = 0;
     form = cgi.FieldStorage()
-    print "<p>username len is ", form["username"].value, "</p>"
-    print "<p>password len is ", form["password"].value, "</p>"
-    print "<p>address len is ", form["address"].value, "</p>"
-    print "<p>city len is ", form["city"].value, "</p>"
-    print "<p>state len is ", form["state"].value, "</p>"
-    print "<p>zipcode len is ", form["zipcode"].value, "</p>"
-    print "<p>email len is ", form["email"].value, "</p>"
-    print "<p>phone len is ", form["phone"].value, "</p>"
+    
+    if form.has_key("username") :
+        strlen = len(form["username"].value)
+    else :
+        strlen = 0;
+        
+    print "<p>username len is ", strlen, "</p>"
+        
+    if form.has_key("password") :
+        strlen = len(form["password"].value)
+    else :
+        strlen = 0;
+    print "<p>password len is ", strlen, "</p>"
+    
+    if form.has_key("address") :
+        strlen = len(form["address"].value)
+    else :
+        strlen = 0;
+
+    print "<p>address len is ", strlen, "</p>"
+
+    if form.has_key("city") :
+        strlen = len(form["city"].value)
+    else :
+        strlen = 0;
+    print "<p>city len is ", strlen, "</p>"
+    
+    if form.has_key("state") :
+        strlen = len(form["state"].value)
+    else :
+        strlen = 0;
+    print "<p>state len is ", strlen, "</p>"
+
+    if form.has_key("zipcode") :
+        strlen = len(form["zipcode"].value)
+    else :
+        strlen = 0;
+    print "<p>zipcode len is ", strlen, "</p>"
+    
+    if form.has_key("email") :
+        strlen = len(form["email"].value)
+    else :
+        strlen = 0;
+    print "<p>email len is ", strlen, "</p>"
+    
+    if form.has_key("phone") :
+        strlen = len(form["phone"].value)
+    else :
+        strlen = 0;
+    print "<p>phone len is ", strlen, "</p>"
 main()
