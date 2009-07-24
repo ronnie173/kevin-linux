0920220e438e59d5b7a9c2fd7b126ac37ed9a4ba
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-07-24 10:04:28 +0000

    type convert

--- trunk/basic/structTest.c
+++ trunk/basic/structTest.c
@@ -3,20 +3,31 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
+#include <strings.h>
 
 typedef struct _name_t {
     char lastName[25];
     char firstName[25];
 } name_t;
 
+int nameStructTest();
+int convertTest();
 int dumpName(const name_t *tmp);
  
 int main(int argc, char *argv[]) {
+    nameStructTest();
+    convertTest();
+    
+    return 0;
+}
 
-    name_t one, two, three, four;
+int nameStructTest() {
+    name_t two, three, four;
 
-    strcpy(one.lastName, "Zhang");
-    strcpy(one.firstName, "Kevin");
+    name_t one = { .lastName = "Zhang", .firstName = "Kevin" };
+    //strcpy(one.lastName, "Zhang");
+    //strcpy(one.firstName, "Kevin");
     dumpName(&one);
 
     two = one;
@@ -34,8 +45,26 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+int convertTest() {
+    char *p = NULL;
+    name_t *tp = NULL;
+    
+    p = malloc(100);
+    bzero(p, 100);
+    tp = (name_t*)p;
+    
+    strcpy(tp->lastName, "Guo");
+    strcpy(tp->firstName, "Zhang");
+    
+    dumpName(tp);
+    
+    return 0;
+}
+
 int dumpName(const name_t *tmp) {
     printf("in file %s, at line %d\n", __FILE__, __LINE__);
+    if (NULL == temp) return 0;
+    
     printf("first: [%s], last: [%s]\n", tmp->firstName, tmp->lastName);
     return 0;
 }
