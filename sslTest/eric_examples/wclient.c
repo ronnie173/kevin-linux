d2587d3e606e09e179a216bb2fcf0925bd4d4e2e
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2009-08-27 09:45:46 +0000

    code format

--- sslTest/eric_examples/wclient.c
+++ sslTest/eric_examples/wclient.c
@@ -16,67 +16,72 @@ static int require_server_auth = 1;
 static int http_request(ssl)
 SSL *ssl;
 {
-    char *request=0;
+    char *request = 0;
     char buf[BUFSIZZ];
     int r;
     int len, request_len;
 
     /* Now construct our HTTP request */
-    request_len=strlen(REQUEST_TEMPLATE)+
-    strlen(host)+6;
-    if(!(request=(char *)malloc(request_len)))
-    err_exit("Couldn't allocate request");
-    snprintf(request,request_len,REQUEST_TEMPLATE,
-            host,port);
+    request_len = strlen(REQUEST_TEMPLATE) + strlen(host) + 6;
+    if (!(request = (char *)malloc(request_len)))
+        err_exit("Couldn't allocate request");
+    
+    snprintf(request, request_len, REQUEST_TEMPLATE, host, port);
 
     /* Find the exact request_len */
-    request_len=strlen(request);
+    request_len = strlen(request);
 
-    r=SSL_write(ssl,request,request_len);
-    switch(SSL_get_error(ssl,r)) {
+    r = SSL_write(ssl, request, request_len);
+    switch (SSL_get_error(ssl, r)) {
         case SSL_ERROR_NONE:
-        if(request_len!=r)
-        err_exit("Incomplete write!");
+        if (request_len != r) err_exit("Incomplete write!");
         break;
+        
         default:
         berr_exit("SSL write problem");
     }
 
     /* Now read the server's response, assuming
      that it's terminated by a close */
-    while(1) {
-        r=SSL_read(ssl,buf,BUFSIZZ);
-        switch(SSL_get_error(ssl,r)) {
+    while (1) {
+        r = SSL_read(ssl, buf, BUFSIZZ);
+        switch (SSL_get_error(ssl, r)) {
             case SSL_ERROR_NONE:
-            len=r;
+            len = r;
             break;
+            
             case SSL_ERROR_ZERO_RETURN:
             goto shutdown;
+            
             case SSL_ERROR_SYSCALL:
-            fprintf(stderr,
-                    "SSL Error: Premature close\n");
+            fprintf(stderr, "SSL Error: Premature close\n");
             goto done;
+            
             default:
             berr_exit("SSL read problem");
         }
 
-        fwrite(buf,1,len,stdout);
+        fwrite(buf, 1, len, stdout);
     }
 
-    shutdown:
-    r=SSL_shutdown(ssl);
-    switch(r) {
+shutdown:
+    r = SSL_shutdown(ssl);
+    switch (r) {
         case 1:
         break; /* Success */
+        
         case 0:
+        
         case -1:
+        
         default:
         berr_exit("Shutdown failed");
     }
 
-    done:
+done:
     SSL_free(ssl);
     free(request);
+
     return(0);
 }
 
@@ -91,37 +96,38 @@ char **argv;
     extern char *optarg;
     int c;
 
-    while((c=getopt(argc,argv,"h:p:i"))!=-1) {
+    while ((c = getopt(argc,argv,"h:p:i")) != -1) {
         switch(c) {
             case 'h':
-            if(!(host=strdup(optarg)))
-            err_exit("Out of memory");
+            if (!(host = strdup(optarg)))
+                err_exit("Out of memory");
             break;
+
             case 'p':
-            if(!(port=atoi(optarg)))
-            err_exit("Bogus port specified");
+            if (!(port = atoi(optarg)))
+                err_exit("Bogus port specified");
             break;
+
             case 'i':
-            require_server_auth=0;
+            require_server_auth = 0;
             break;
         }
     }
 
     /* Build our SSL context*/
-    ctx=initialize_ctx(KEYFILE,PASSWORD);
+    ctx = initialize_ctx(KEYFILE, PASSWORD);
 
     /* Connect the TCP socket*/
-    sock=tcp_connect(host,port);
+    sock = tcp_connect(host, port);
 
     /* Connect the SSL socket */
-    ssl=SSL_new(ctx);
-    sbio=BIO_new_socket(sock,BIO_NOCLOSE);
-    SSL_set_bio(ssl,sbio,sbio);
+    ssl = SSL_new(ctx);
+    sbio = BIO_new_socket(sock, BIO_NOCLOSE);
+    SSL_set_bio(ssl, sbio, sbio);
 
-    if(SSL_connect(ssl)<=0)
-    berr_exit("SSL connect error");
-    if(require_server_auth)
-    check_cert(ssl,host);
+    if (SSL_connect(ssl) <= 0) berr_exit("SSL connect error");
+    
+    if (require_server_auth) check_cert(ssl, host);
 
     /* Now make our HTTP request */
     http_request(ssl);
