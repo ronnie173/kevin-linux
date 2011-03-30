f2f7e00a7491fce7c4d77f4ecba25504500a5469
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2011-03-30 09:28:07 +0000

    add sqlite test

---
+++ trunk/SqliteTest/create_table.sql
@@ -0,0 +1,67 @@
+drop table if exists continent_mapping;
+drop table if exists country_mapping;
+drop table if exists state_mapping;
+drop table if exists city_mapping;
+drop table if exists host_mapping;
+drop table if exists url_mapping;
+drop table if exists attack_mapping;
+drop table if exists main_log;
+
+create table continent_mapping (
+    id int1 primary key asc,
+    name character(20) not null
+);
+     
+create table country_mapping (
+    id int2 primary key asc,
+    name character(40) not null
+);
+
+create table state_mapping (
+    id int4 primary key asc,
+    name character(40) not null
+);
+
+create table city_mapping (
+    id int4 primary key asc,
+    name character(40) not null
+);
+
+create table host_mapping (
+    id int4 primary key asc,
+    name character(128) not null
+);
+
+create table url_mapping (
+    id int4 primary key asc,
+    name character(256) not null
+);
+
+create table attack_mapping (
+    id int2 primary key asc,
+    name character(64) not null
+);
+
+create table main_log (
+    id int8 primary key asc,
+    timestamp datetime not null,
+    src_ip int4 not null,
+    src_port int2 not null,
+    dst_ip int4 not null,
+    dst_port int2 not null,
+    bandwidth int2 not null,
+    
+    /* don't use foreign key for now */
+    continent_id int1 not null,
+    country_id int2 not null,
+    state_id int4,
+    city_id int4,
+    host_id int4 not null,
+    url_id int4 not null,
+    attack_id int2 not null
+);
+
+    
+    
+    
+    
---
+++ trunk/SqliteTest/populate_table.sql
@@ -0,0 +1,67 @@
+/* populate continent table */
+insert into continent_mapping values (1, 'North America');
+insert into continent_mapping values (2, 'South America');
+insert into continent_mapping values (3, 'Antarctica');
+insert into continent_mapping values (4, 'Africa');
+insert into continent_mapping values (5, 'Europe');
+insert into continent_mapping values (6, 'Asia');
+insert into continent_mapping values (7, 'Australia');
+
+/* populate country table */
+insert into country_mapping values (1, 'USA');
+insert into country_mapping values (2, 'China');
+insert into country_mapping values (3, 'France');
+insert into country_mapping values (4, 'Japan');
+insert into country_mapping values (5, 'German');
+insert into country_mapping values (6, 'Korea');
+insert into country_mapping values (7, 'Italy');
+insert into country_mapping values (8, 'Canada');
+insert into country_mapping values (9, 'Russia');
+insert into country_mapping values (10, 'UK');
+
+/* populate state table */
+insert into state_mapping values (1, 'CA');
+insert into state_mapping values (2, 'OU');
+insert into state_mapping values (3, 'WA');
+insert into state_mapping values (4, 'NY');
+insert into state_mapping values (5, 'NT');
+insert into state_mapping values (6, 'NV');
+
+/* populate city table */
+insert into city_mapping values (1, 'Beijing');
+insert into city_mapping values (2, 'Shanghai');
+insert into city_mapping values (3, 'Tianjin');
+insert into city_mapping values (4, 'Chengdu');
+insert into city_mapping values (5, 'Hongkong');
+
+/* populate host table */
+insert into host_mapping values (1, 'www.sina.com.cn');
+insert into host_mapping values (2, 'www.ibm.com');
+insert into host_mapping values (3, 'www.nba.com');
+insert into host_mapping values (4, 'www.fortinet.com');
+insert into host_mapping values (5, 'www.hp.com');
+insert into host_mapping values (6, 'www.icsa.com');
+insert into host_mapping values (7, 'www.yahoo.com');
+insert into host_mapping values (8, 'www.google.com');
+
+/* populate url table */
+insert into url_mapping values (1, '/index.html');
+insert into url_mapping values (2, '/index.asp');
+insert into url_mapping values (3, '/index.jsp');
+insert into url_mapping values (4, '/index.do');
+insert into url_mapping values (5, '/index.action');
+insert into url_mapping values (6, '/home.html');
+insert into url_mapping values (7, '/test.html');
+insert into url_mapping values (8, '/hello.html');
+insert into url_mapping values (9, '/hello.php');
+
+/* populate attack table */
+insert into attack_mapping values (1, 'xss_1');
+insert into attack_mapping values (2, 'xss_2');
+insert into attack_mapping values (3, 'xss_3');
+insert into attack_mapping values (4, 'xss_4');
+insert into attack_mapping values (5, 'sql_1');
+insert into attack_mapping values (6, 'sql_2');
+insert into attack_mapping values (7, 'sql_3');
+insert into attack_mapping values (8, 'sql_4');
+
