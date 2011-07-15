df1c3159cca7acd12a7b74fd612ced74aeb9241c
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2011-07-15 07:02:47 +0000

    netfilter script

---
+++ netfilter/my_fw.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# Parameter settings
+  EXTIF="eth1"           # To public interface
+  INIF="eth0"            # To internal interface
+  INNET="192.168.1.0/24" # Internal subnet
+  export EXTIF INIF INNET
+
+# Part one, firewall unit settings
+# 1. kernel network settings
+#  echo "1" > /proc/sys/net/ipv4/tcp_syncookies
+#  echo "1" > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+#  for i in /proc/sys/net/ipv4/conf/*/{rp_filter,log_martians}; do
+#        echo "1" > $i
+#  done
+#  for i in /proc/sys/net/ipv4/conf/*/{,accept_source_route,accept_redirects,\
+#send_redirects}; do
+#        echo "0" > $i
+#  done
+
+# 2. clear rule, predefine policy and lo related settings
+  PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin; export PATH
+  iptables -F
+  iptables -X
+  iptables -Z
+  iptables -P INPUT   DROP
+  iptables -P OUTPUT  ACCEPT
+  iptables -P FORWARD ACCEPT
+  iptables -A INPUT -i lo -j ACCEPT
+  iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# 3. apply additional firewall scripts
+#  if [ -f /usr/local/virus/iptables/iptables.deny ]; then
+#        sh /usr/local/virus/iptables/iptables.deny
+#  fi
+#  if [ -f /usr/local/virus/iptables/iptables.allow ]; then
+#        sh /usr/local/virus/iptables/iptables.allow
+#  fi
+#  if [ -f /usr/local/virus/httpd-err/iptables.http ]; then
+#        sh /usr/local/virus/httpd-err/iptables.http
+#  fi
+
+# 4. allow certain ICMP
+  AICMP="0 3 3/4 4 11 12 14 16 18"
+  for tyicmp in $AICMP
+  do
+    iptables -A INPUT -i $EXTIF -p icmp --icmp-type $tyicmp -j ACCEPT
+  done
+
+# 5. allow certain services
+# iptables -A INPUT -p TCP -i $EXTIF --dport  21 --sport 1024:65534 -j ACCEPT # FTP
+# iptables -A INPUT -p TCP -i $EXTIF --dport  22 --sport 1024:65534 -j ACCEPT # SSH
+# iptables -A INPUT -p TCP -i $EXTIF --dport  25 --sport 1024:65534 -j ACCEPT # SMTP
+# iptables -A INPUT -p UDP -i $EXTIF --dport  53 --sport 1024:65534 -j ACCEPT # DNS
+# iptables -A INPUT -p TCP -i $EXTIF --dport  53 --sport 1024:65534 -j ACCEPT # DNS
+# iptables -A INPUT -p TCP -i $EXTIF --dport  80 --sport 1024:65534 -j ACCEPT # WWW
+# iptables -A INPUT -p TCP -i $EXTIF --dport 110 --sport 1024:65534 -j ACCEPT # POP3
+# iptables -A INPUT -p TCP -i $EXTIF --dport 443 --sport 1024:65534 -j ACCEPT # HTTPS
+
+
+# Part two, firewall setting for internal network
+# 1. load modules
+#  modules="ip_tables iptable_nat ip_nat_ftp ip_nat_irc ip_conntrack 
+#ip_conntrack_ftp ip_conntrack_irc"
+#  for mod in $modules
+#  do
+#      testmod=`lsmod | grep "^${mod} " | awk '{print $1}'`
+#      if [ "$testmod" == "" ]; then
+#            modprobe $mod
+#      fi
+#  done
+
+# 2. clear NAT table rules
+  iptables -F -t nat
+  iptables -X -t nat
+  iptables -Z -t nat
+  iptables -t nat -P PREROUTING  ACCEPT
+  iptables -t nat -P POSTROUTING ACCEPT
+  iptables -t nat -P OUTPUT      ACCEPT
+
+# 3. turn on routing
+  if [ "$INIF" != "" ]; then
+    iptables -A INPUT -i $INIF -j ACCEPT
+    echo "1" > /proc/sys/net/ipv4/ip_forward
+    if [ "$INNET" != "" ]; then
+        for innet in $INNET
+        do
+            iptables -t nat -A POSTROUTING -s $innet -o $EXTIF -j MASQUERADE
+        done
+    fi
+  fi
+  # If there are connection problems to MSN or certain website£¬
+  # that could be a MTU issue, uncomment following line to load MTU limits
+  # iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss \
+  #          --mss 1400:1536 -j TCPMSS --clamp-mss-to-pmtu
+
+# 4. Expose server service to outside
+# iptables -t nat -A PREROUTING -p tcp -i $EXTIF --dport 80 \
+#          -j DNAT --to-destination 192.168.1.210:80 # WWW
+
+# 5. special requirements, such as Windows terminal
+# iptables -t nat -A PREROUTING -p tcp -s 1.2.3.4  --dport 6000 \
+#          -j DNAT --to-destination 192.168.1.100
+# iptables -t nat -A PREROUTING -p tcp -s 1.2.3.4  --sport 3389 \
+#          -j DNAT --to-destination 192.168.1.100
+
+# 6. save the config
+  /etc/init.d/iptables save
