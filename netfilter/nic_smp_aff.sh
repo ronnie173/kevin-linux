0b2fa59e511212508a80a8da37efa09feb67d8fb
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2011-07-26 10:21:46 +0000

    lab env

--- netfilter/nic_smp_aff.sh
+++ netfilter/nic_smp_aff.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Parameter settings
-  EXTIF="eth0"           # To public interface
-  EXTIF_IRQ="46"         # To public interface irq
+  EXTIF="eth1"           # To public interface
+  EXTIF_IRQ="16"         # To public interface irq
   INIF="eth2"            # To internal interface
-  INIF_IRQ="44"          # To internal interface irq
+  INIF_IRQ="17"          # To internal interface irq
   export EXTIF INIF EXTIF_IRQ INIF_IRQ
 
 # Part one, set smp affinity
 # let cpu0 and cpu1 handle extif irq
 echo "3" > /proc/irq/$EXTIF_IRQ/smp_affinity
+echo "/proc/irq/$EXTIF_IRQ/smp_affinity"
+cat /proc/irq/$EXTIF_IRQ/smp_affinity
 
 # let cpu2 and cpu3 handle inif irq
 echo "c" > /proc/irq/$INIF_IRQ/smp_affinity
-
+echo "/proc/irq/$INIF_IRQ/smp_affinity"
+cat /proc/irq/$INIF_IRQ/smp_affinity
