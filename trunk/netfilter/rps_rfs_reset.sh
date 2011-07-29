df16a57180470c42d1fdcc1bce2dfd884d6462a9
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2011-07-29 05:13:21 +0000

    new script

--- trunk/netfilter/rps_rfs_reset.sh
+++ trunk/netfilter/rps_rfs_reset.sh
@@ -9,14 +9,26 @@
 # let cpu0 and cpu1 handle extif irq
 echo "0" > /sys/class/net/$EXTIF/queues/rx-0/rps_cpus
 echo "0" > /sys/class/net/$EXTIF/queues/tx-0/xps_cpus
-echo "$EXTIF rx-0/rps_cpus tx-0/xps_cpus"
+echo "0" > /sys/class/net/$EXTIF/queues/rx-0/rps_flow_cnt
+
+echo "$EXTIF rx-0/rps_cpus tx-0/xps_cpus rx-0/rps_flow_cnt"
 cat /sys/class/net/$EXTIF/queues/rx-0/rps_cpus
 cat /sys/class/net/$EXTIF/queues/tx-0/xps_cpus
+cat /sys/class/net/$EXTIF/queues/rx-0/rps_flow_cnt
 
 # let cpu2 and cpu3 handle inif irq
 echo "0" > /sys/class/net/$INIF/queues/rx-0/rps_cpus
 echo "0" > /sys/class/net/$INIF/queues/tx-0/xps_cpus
-echo "$INIF rx-0/rps_cpus tx-0/xps_cpus"
+echo "0" > /sys/class/net/$INIF/queues/rx-0/rps_flow_cnt
+
+echo "$INIF rx-0/rps_cpus tx-0/xps_cpus rx-0/rps_flow_cnt"
 cat /sys/class/net/$INIF/queues/rx-0/rps_cpus
 cat /sys/class/net/$INIF/queues/tx-0/xps_cpus
+cat /sys/class/net/$INIF/queues/rx-0/rps_flow_cnt
+
+# unset /proc/sys/net/core
+echo "0" > /proc/sys/net/core/rps_sock_flow_entries
+echo "rps_sock_flow_entries"
+cat /proc/sys/net/core/rps_sock_flow_entries
+
 
--- trunk/netfilter/rps_rfs_set.sh
+++ trunk/netfilter/rps_rfs_set.sh
@@ -11,18 +11,28 @@
 #echo "3" > /sys/class/net/$EXTIF/queues/tx-0/xps_cpus
 echo "f" > /sys/class/net/$EXTIF/queues/rx-0/rps_cpus
 echo "f" > /sys/class/net/$EXTIF/queues/tx-0/xps_cpus
+echo "4096" > /sys/class/net/$EXTIF/queues/rx-0/rps_flow_cnt
 
-echo "$EXTIF rx-0/rps_cpus tx-0/xps_cpus"
+echo "$EXTIF rx-0/rps_cpus tx-0/xps_cpus rx-0/rps_flow_cnt"
 cat /sys/class/net/$EXTIF/queues/rx-0/rps_cpus
 cat /sys/class/net/$EXTIF/queues/tx-0/xps_cpus
+cat /sys/class/net/$EXTIF/queues/rx-0/rps_flow_cnt
 
 # let cpu2 and cpu3 handle inif irq
 #echo "c" > /sys/class/net/$INIF/queues/rx-0/rps_cpus
 #echo "c" > /sys/class/net/$INIF/queues/tx-0/xps_cpus
 echo "f" > /sys/class/net/$INIF/queues/rx-0/rps_cpus
 echo "f" > /sys/class/net/$INIF/queues/tx-0/xps_cpus
+echo "4096" > /sys/class/net/$INIF/queues/rx-0/rps_flow_cnt
 
-echo "$INIF rx-0/rps_cpus tx-0/xps_cpus"
+echo "$INIF rx-0/rps_cpus tx-0/xps_cpus rx-0/rps_flow_cnt"
 cat /sys/class/net/$INIF/queues/rx-0/rps_cpus
 cat /sys/class/net/$INIF/queues/tx-0/xps_cpus
+cat /sys/class/net/$INIF/queues/rx-0/rps_flow_cnt
+
+# set /proc/sys/net/core
+echo "4096" > /proc/sys/net/core/rps_sock_flow_entries
+echo "rps_sock_flow_entries"
+cat /proc/sys/net/core/rps_sock_flow_entries
+
 
