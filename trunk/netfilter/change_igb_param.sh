0d7b9bd37855438e30479b5f1c6245006be7cbd7
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2011-08-04 06:14:11 +0000

    update

--- trunk/netfilter/change_igb_param.sh
+++ trunk/netfilter/change_igb_param.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 rmmod igb
-modprobe igb InterruptThrottleRate=8000,8000 RSS=4,4 IntMode=2,2 QueuePairs=0,0
+sleep 2
+
+modprobe igb InterruptThrottleRate=8000,8000 RSS=4,4 IntMode=2,2 QueuePairs=1,1
+sleep 2
+
 /etc/init.d/networking stop
+sleep 5
+
 /etc/init.d/networking start
+sleep 5
+
 cat /proc/interrupts
 
