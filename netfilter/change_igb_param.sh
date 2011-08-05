3906947459a7a9e4f2bf63d428d1956036c08389
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2011-08-05 06:51:39 +0000

    update

--- netfilter/change_igb_param.sh
+++ netfilter/change_igb_param.sh
@@ -7,7 +7,7 @@ echo "list modules after rmmod"
 lsmod | grep igb
 sleep 1
 
-modprobe igb InterruptThrottleRate=5000,5000 RSS=0,0 IntMode=2,2 QueuePairs=1,1
+modprobe igb InterruptThrottleRate=5000,5000 RSS=0,0 IntMode=2,2 QueuePairs=0,0
 sleep 2
 
 echo "list modules after modprobe"
