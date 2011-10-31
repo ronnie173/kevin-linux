29e048b6fcf28cc2556ede22b0f0bf39e0d43a06
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2011-10-31 14:31:05 +0000

    perl test

---
+++ trunk/PerlTest/re_test.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -W
+#
+# This is a regular expression test.
+#
+# Authors: zhangjw@fortinet.com
+#
+###########################################
+
+use strict;
+use warnings;
+
+sub main {
+    my $tmp = "FG43443 login:";
+    my $result = $tmp =~ m/.*login:/;
+    
+    print $result . "\n";
+    
+    return 0;
+}
+
+main();
