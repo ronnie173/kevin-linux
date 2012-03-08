3c4c6b3d2d043c0736f1588b10a7770dcaff0559
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2012-03-08 05:35:20 +0000

    array code update

--- basic/arrayTest.c
+++ basic/arrayTest.c
@@ -1,11 +1,14 @@
 /*
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX 10
 int main(int argc, char *argv[]) {
     int i = 2, a[MAX], j = 23;
     char *p = "hello world.";
+    //char p[] = "hello world.";
     char *q = "hello world.";
     
     if (&i == &a[MAX]) {
@@ -21,10 +24,30 @@ int main(int argc, char *argv[]) {
     printf("p is [%s]\n", p);
     printf("q is [%s]\n", q);
     
-    p[4] = '\0';
-    
+    //p[4] = '\0';
+
+    p = (char *)malloc(20);
+    strncpy(p, "hello world", 20);
     printf("p is [%s]\n", p);
-    printf("q is [%s]\n", q);
+
+/*    char *tmp = p + 4;
+    printf("4 is %s\n", tmp);
+    printf("4 char is [%c]\n", *tmp);
+    *tmp = '\0';
+*/
+    p[4] = '\0';
+    printf("p is [%s]\n", p);
+    free(p);
+
+    const char *strArray[3] = {
+        [0] = "hello",
+        [1] = "world",
+        [2] = "kevin"
+    };
+
+    for (i = 0; i < 3; i++) {
+        printf("index [%d], str [%s]\n", i, strArray[i]);
+    }
 
     return 0;
 }
