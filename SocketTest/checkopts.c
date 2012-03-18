634894d78bcd6ceae487dbce8c81bc14ce66fd00
zhangjw <zhangjw@5525cbca-0a00-11de-b025-799c3e7cca98>
2012-03-18 06:22:21 +0000

    more options

--- SocketTest/checkopts.c
+++ SocketTest/checkopts.c
@@ -2,12 +2,16 @@
 #include <sys/socket.h>
 //#include <linux/in6.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <linux/in.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+//#include <arpa/inet.h>
 //#include <netinet/tcp.h>       /* for TCP_xxx defines */
 #include <linux/tcp.h>
 #include <linux/udp.h>
+//#include <linux/ip.h>
 
 union val {
     int               i_val;
@@ -29,13 +33,28 @@ struct sock_opts {
     int         opt_name;
     char        *(*opt_val_str) (union val *, int);
 } sock_opts[] = {
+    // SO_BINDTODEVICE: bind socket to a particular device like "eht0"
+    // SO_BSDCOMPAT; enable BSD bug-to-bug compatiblity
+    // SO_PEERCRED: return the credentials of foreign process
+    // SO_PRIORITY: set the priority for all packets to be sent
+    // SO_RCVBUFFORCE
+    // SO_RCVLOWAT
+    // SO_SNDLOWAT
+    // SO_RCVTIMEO
+    // SO_SNDTIMEO
+    // SO_SNDBUFFORCE
+    //
+    { "SO_ACCEPTCONN",       SOL_SOCKET, SO_ACCEPTCONN,  sock_str_flag },
     { "SO_BROADCAST",        SOL_SOCKET, SO_BROADCAST,   sock_str_flag },
     { "SO_DEBUG",            SOL_SOCKET, SO_DEBUG,       sock_str_flag },
+    { "SO_DOMAIN",           SOL_SOCKET, SO_DOMAIN,      sock_str_int },
     { "SO_DONTROUTE",        SOL_SOCKET, SO_DONTROUTE,   sock_str_flag },
     { "SO_ERROR",            SOL_SOCKET, SO_ERROR,       sock_str_int },
     { "SO_KEEPALIVE",        SOL_SOCKET, SO_KEEPALIVE,   sock_str_flag },
     { "SO_LINGER",           SOL_SOCKET, SO_LINGER,      sock_str_linger },
     { "SO_OOBINLINE",        SOL_SOCKET, SO_OOBINLINE,   sock_str_flag },
+    { "SO_PASSCRED",         SOL_SOCKET, SO_PASSCRED,    sock_str_flag },
+    { "SO_PROTOCOL",         SOL_SOCKET, SO_PROTOCOL,    sock_str_int },
     { "SO_RCVBUF",           SOL_SOCKET, SO_RCVBUF,      sock_str_int },
     { "SO_SNDBUF",           SOL_SOCKET, SO_SNDBUF,      sock_str_int },
     { "SO_RCVLOWAT",         SOL_SOCKET, SO_RCVLOWAT,    sock_str_int },
@@ -48,8 +67,27 @@ struct sock_opts {
 #else
     { "SO_REUSEPORT",        0,          0,              NULL },
 #endif
+    { "SO_TIMESTAMP",        SOL_SOCKET, SO_TIMESTAMP,   sock_str_flag },
     { "SO_TYPE",             SOL_SOCKET, SO_TYPE,        sock_str_int },
 //    { "SO_USELOOPBACK",      SOL_SOCKET, SO_USELOOPBACK, sock_str_flag },
+    // IP related options
+    // IP_ADD_MEMBERSHIP: join a multicast group
+    // IP_DROP_MEMBERSHIP: leave a multicast group
+    // IP_HDRINCL: user supplies an IP header
+    // IP_MTU: retrieve the current known path MTU. only valid when connected
+    // IP_MTU_DISCOVER: path mtu discovery
+    // IP_MULTICAST_IF: set the local device for a multicast socket.
+    // IP_OPTIONS: 40 bytes for IPv4
+    // IP_PKTINFO: struct in_pktinfo
+    // IP_RECVOPTS: pass all incoming IP options to the user
+    // IP_RETOPTS: identical to IP_RECVOPTS, but return raw options
+    { "IP_MULTICAST_LOOP",   IPPROTO_IP, IP_MULTICAST_LOOP, sock_str_flag },
+    { "IP_MULTICAST_TTL",    IPPROTO_IP, IP_MULTICAST_TTL, sock_str_int },
+    { "IP_NODEFRAG",         IPPROTO_IP, IP_NODEFRAG,    sock_str_flag },
+    { "IP_RECVERR",          IPPROTO_IP, IP_RECVERR,     sock_str_flag },
+    { "IP_RECVTOS",          IPPROTO_IP, IP_RECVTOS,     sock_str_flag },
+    { "IP_RECVTTL",          IPPROTO_IP, IP_RECVTTL,     sock_str_int },
+//    { "IP_ROUTER_ALERT",     IPPROTO_IP, IP_ROUTER_ALERT, sock_str_flag },
     { "IP_TOS",              IPPROTO_IP, IP_TOS,         sock_str_int },
     { "IP_TTL",              IPPROTO_IP, IP_TTL,         sock_str_int },
 //    { "IPV6_DONTFRAG",       IPPROTO_IPV6,IPV6_DONTFRAG, sock_str_flag },
@@ -84,9 +122,11 @@ int main(int argc, char **argv) {
             printf("(undefined)\n");
         } else {
             switch (ptr->opt_level) {
-            case SOL_SOCKET:
             case IPPROTO_IP:
+//                fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+//                break;
             case IPPROTO_TCP:
+            case SOL_SOCKET:
                 fd = socket(AF_INET, SOCK_STREAM, 0);
                 break;
 #ifdef  IPV6
@@ -110,7 +150,7 @@ int main(int argc, char **argv) {
 
             len = sizeof(val);
             if (getsockopt(fd, ptr->opt_level, ptr->opt_name, &val, &len) == -1) {
-                 printf("getsockopt error");
+                 printf("getsockopt error: %s\n", strerror(errno));
                  return 1; 
              } else {
                  printf("default = %s\n", (*ptr->opt_val_str)  (&val, len));
